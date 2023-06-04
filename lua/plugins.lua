eb3253699b78c5fcaec6d0dcfc1ba472f587e212
arrKhange1 <therock65frostik@gmail.com>
2023-06-04 20:25:59 +0500

    feat: add web dev icons

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,7 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'kyazdani42/nvim-web-devicons'
 use {
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
